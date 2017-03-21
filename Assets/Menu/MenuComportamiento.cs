4a05a3893dd27ec6a8a2c9b9c21897b1e881e445
rgonzalezs <rgonzalezs@gmail.com>
2017-03-21 10:50:12 -0300

    Migracion de sistema previo con menu incluido

---
+++ Assets/Menu/MenuComportamiento.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MenuComportamiento : MonoBehaviour {
+
+	public int iEstado;
+
+	// Use this for initialization
+	void Start () {
+		iEstado = 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void MuestraMenu(){
+		if (iEstado == 2) {
+			GetComponent<Animation>().Play ("MenuIn");
+			iEstado = 1;
+		} 
+		else {
+			GetComponent<Animation>().Play ("MenuOut");
+			iEstado = 2;
+		}
+	}
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# SolAR
-App based on solar system using unity and augmented reality on mobile devices
